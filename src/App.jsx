6663cff26d52ad2a659fd0969b188702c8382c37
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2023-11-11 20:43:45 +0300

    orginizing file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
+import CoreConcept from "./components/CoreConcept.jsx";
+import Header from "./components/Header.jsx";
 import { CORE_CONCEPTS } from "./data.js";
-
-const reactDescriptions = ["Fundamental", "Cruical", "Core"];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-function Header() {
-  const description = reactDescriptions[getRandomInt(3)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
