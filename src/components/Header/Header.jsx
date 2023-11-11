eed84ce27b8b2b63fd7480d59f00a10f1e31e5d0
ahmet sarikaya <ahmetsarikaya0696@gmail.com>
2023-11-11 20:56:43 +0300

    orginizing component and css file together

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import reactImg from "../assets/react-core-concepts.png";
+import reactImg from "../../assets/react-core-concepts.png";
+import classes from "./Header.module.css";
 
 const reactDescriptions = ["Fundamental", "Cruical", "Core"];
 
@@ -10,7 +11,7 @@ function Header() {
   const description = reactDescriptions[getRandomInt(3)];
 
   return (
-    <header>
+    <header className={classes.header}>
       <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
